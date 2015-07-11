6f6adcde56f8890f8116a84fb2ecc5ef368ed9e1
Masamoto Miyata <miyata@sincere-co.com>
2015-07-12 02:54:28 +0900

    保存後の確認を実装。

--- tests/phpunit/I18nTest.php
+++ tests/phpunit/I18nTest.php
@@ -35,7 +35,7 @@ class I18nTest extends PHPUnit_Framework_TestCase
     return Yaml::parse(file_get_contents($this->basePath.'/sample/lang/'.$lang.'.yml'));
   }
 
-  public function testGenerator()
+  public function testSimpleGenerator()
   {
     $gen = new Gomo\I18n\Generator();
     $gen
@@ -50,10 +50,24 @@ class I18nTest extends PHPUnit_Framework_TestCase
     $gen->addEntries($this->basePath.'/sample/app/controllers/TestController.php');
 
     $enties = $gen->getEntries();
-    $this->assertEquals(2, count($enties));
+    $this->assertCount(2, $enties);
 
-    $this->assertEquals(1, count($enties['保存']->getFiles()));
+    $this->assertCount(1, $enties['保存']->getFiles());
 
     $gen->save();
+
+    //保存ファイルの確認
+    $result = $this->loadYaml('ja');
+    $this->assertCount(2, $result);
+
+    $keys = array_keys($result);
+    $this->assertEquals('保存', $keys[0]);
+    $this->assertEquals('戻る', $keys[1]);
+
+    $this->assertCount(1, $result['保存']['files']);
+    $this->assertEquals($this->basePath.'/sample/app/controllers/TestController.php', $result['保存']['files'][0]);
+
+    $this->assertCount(1, $result['戻る']['files']);
+    $this->assertEquals($this->basePath.'/sample/app/controllers/TestController.php', $result['戻る']['files'][0]);
   }
 }
