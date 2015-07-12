0ee94c302ed6b17b9d26ab6fa5bc6a7a5b3a8c22
Masamoto Miyata <miyata@sincere-co.com>
2015-07-13 03:18:24 +0900

    変な正規表現のチェックを追加。言語ファイルのリセット部分をメソッドに抽出。

--- tests/phpunit/I18nTest.php
+++ tests/phpunit/I18nTest.php
@@ -14,20 +14,6 @@ class I18nTest extends PHPUnit_Framework_TestCase
   public function setUp()
   {
     $this->basePath = __DIR__ . '/..';
-
-    $gen = new Gomo\I18n\Generator();
-    $gen
-      ->setSourceLang('ja')
-      ->setDir($this->basePath.'/sample/lang');
-
-    file_put_contents($gen->getLangFilePath(), Yaml::dump(array(
-      "保存" => array(
-        "value" => "",
-        "files" => array(
-          $this->basePath.'/sample/index.php',
-        ),
-      ),
-    )), Gomo\I18n\Generator::YAML_INLINE);
   }
 
   private function loadYaml($lang)
@@ -35,8 +21,26 @@ class I18nTest extends PHPUnit_Framework_TestCase
     return Yaml::parse(file_get_contents($this->basePath.'/sample/lang/'.$lang.'.yml'));
   }
 
+  private function resetLangFile($lang, array $values)
+  {
+    $gen = new Gomo\I18n\Generator();
+    $gen
+      ->setSourceLang($lang)
+      ->setDir($this->basePath.'/sample/lang');
+
+    file_put_contents($gen->getLangFilePath(), Yaml::dump($values), Gomo\I18n\Generator::YAML_INLINE);
+  }
+
   public function testSimpleGenerator()
   {
+    $this->resetLangFile('ja', array(
+      "保存" => array(
+        "value" => "",
+        "files" => array(
+          $this->basePath.'/sample/old.php',
+        ),
+      ),
+    ));
     $gen = new Gomo\I18n\Generator();
     $gen
       ->setSourceLang('ja')
@@ -55,7 +59,7 @@ class I18nTest extends PHPUnit_Framework_TestCase
     $entries = $gen->getEntries();
     $this->assertCount(2, $entries);
 
-    $this->assertCount(1, $entries['保存']->getFiles());
+    $this->assertCount(2, $entries['保存']->getFiles());
 
     $gen->save();
 
@@ -67,10 +71,54 @@ class I18nTest extends PHPUnit_Framework_TestCase
     $this->assertEquals('保存', $keys[0]);
     $this->assertEquals('戻る', $keys[1]);
 
-    $this->assertCount(1, $result['保存']['files']);
+    $this->assertCount(2, $result['保存']['files']);
     $this->assertEquals($this->basePath.'/sample/index.php', $result['保存']['files'][0]);
+    $this->assertEquals($this->basePath.'/sample/old.php', $result['保存']['files'][1]);
 
     $this->assertCount(1, $result['戻る']['files']);
     $this->assertEquals($this->basePath.'/sample/index.php', $result['戻る']['files'][0]);
+  }
+
+  public function testRegexGenerator()
+  {
+    $this->resetLangFile('ja', array(
+      "保存" => array(
+        "value" => "",
+        "files" => array(
+          $this->basePath.'/sample/old.php',
+        ),
+      ),
+    ));
+    $gen = new Gomo\I18n\Generator();
+    $gen
+      ->setSourceLang('ja')
+      ->setDir($this->basePath.'/sample/lang');
+
+    $gen->load();
+
+    $entries = $gen->getEntries();
+    $this->assertEquals(1, count($entries));
+
+    //sample/index.phpを読み込む
+    $gen->addEntries($this->basePath.'/sample/regex.php');
+
+    $entries = $gen->getEntries();
+    $this->assertCount(2, $entries);
+
+    $gen->save();
+
+    //保存ファイルの確認
+    $result = $this->loadYaml('ja');
+    $this->assertCount(2, $result);
+
+    $keys = array_keys($result);
+    $this->assertEquals("\$i18n->__i18n('foo')", $keys[0]);
+    $this->assertEquals('保存', $keys[1]);
+
+    $this->assertCount(1, $result["\$i18n->__i18n('foo')"]['files']);
+    $this->assertEquals($this->basePath.'/sample/regex.php', $result["\$i18n->__i18n('foo')"]['files'][0]);
+
+    $this->assertCount(1, $result['保存']['files']);
+    $this->assertEquals($this->basePath.'/sample/old.php', $result['保存']['files'][0]);
   }
 }
---
+++ tests/sample/regex.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <!-- generator supported -->
+  <div><?php echo $i18n->__i18n("$i18n->__i18n('foo')"); ?></div>
+</body>
+</html>
